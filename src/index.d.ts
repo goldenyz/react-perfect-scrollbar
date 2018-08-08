3d28b10bbcbc39771ab0dd30cbc3bd99e4d7f14f
Saffa Shujah <saffashujah2249@gmail.com>
2018-08-08 10:40:31 +0500

    Update containerRef function to take function with param 'ref'
    contaienrRef function is not compatible with (ref) => void as it is taking 
    `containerRef?: ((ref) => void);`  in index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -16,7 +16,7 @@ export interface ScrollBarProps {
     /**
      * get the container ref
      */
-    containerRef?: (() => void);
+    containerRef?: ((ref) => void);
 
     /**
      * fires when the y-axis is scrolled in either direction.
